c4bd025f68d6b6e2d1e137f4d7ffc73dfd7d224d
Mikhail Novik <novik.misha.1@gmail.com>
2024-06-30 16:22:46 +0300

    fix buffer cmp

--- lua/packages/plugins/cmp.lua
+++ lua/packages/plugins/cmp.lua
@@ -4,10 +4,12 @@ return {
     dependencies = {
         "onsails/lspkind.nvim",
         "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
         "L3MON4D3/LuaSnip",
         "saadparwaiz1/cmp_luasnip",
         "rafamadriz/friendly-snippets",
     },
+
     config = function()
         local cmp = require("cmp")
         local lspkind = require("lspkind")
